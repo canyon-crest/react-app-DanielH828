a0f98c43dc7fc132bfb2d8fce143d3f4335988f2
hed3272 <hed3272@my.sduhsd.net>
2026-03-16 13:26:19 -0700

    3/16 Commit for Shop Layout

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,4 +1,4 @@
-import './Card.css'
+import './Shop.css'
 
 import{ useState } from 'react'
 
@@ -8,12 +8,14 @@ function Shop({myname,description, cost}) {
 
     return (
         <div className='myCard'>
-            <h2>{myname}</h2> 
-            <p>Description: {description} Cost: ${cost}</p>
+            <h2 itemID='name'>{myname}</h2> 
+            <p itemID='Description'>Description: {description} Cost: ${cost}</p>
             <button className='ShopButton'onClick={() => setCount(count+1)}>Add {myname} to cart</button>
             <button className='ShopButton'onClick={() => setCount(0)}>Reset</button>
-            <p>Total {myname}: {count}</p>
-            <p>Total Cost of item: {count*cost}</p>
+            <div className="Cost">
+                <p>Total {myname}: {count}</p>
+                <p>Total Cost of item: {count*cost}</p>
+            </div>
         </div>
     )
 }
